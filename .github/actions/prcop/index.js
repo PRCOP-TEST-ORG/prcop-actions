ad17f1b4f754d2ea37574425c7dcb760d952c21a
PHILIP OYOO <oyooempire@gmail.com>
2023-09-28 11:24:18 +0300

    test

--- .github/actions/prcop/index.js
+++ .github/actions/prcop/index.js
@@ -9811,18 +9811,39 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+    // load approvers from the config file
+    let teamMembers_file = await octokit.repos.getContent({
+      owner,
+      repo,
+      path: ".github/approvers.json",
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+        accept: "application/json",
+      },
+    });
+
+    let teamMembers = JSON.parse(
+      Buffer.from(teamMembers_file.data.content, "base64").toString("utf-8")
+    );
+
+    console.log(teamMembers);
+
+    const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+    console.log(`GITHUB_TOKEN: ${GITHUB_TOKEN}`);
+    const { context = {} } = github;
+    const { pull_request } = context.payload;
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
 
 })();
 
--- scripts/prcop/index.js
+++ scripts/prcop/index.js
@@ -1,15 +1,36 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+    // load approvers from the config file
+    let teamMembers_file = await octokit.repos.getContent({
+      owner,
+      repo,
+      path: ".github/approvers.json",
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+        accept: "application/json",
+      },
+    });
+
+    let teamMembers = JSON.parse(
+      Buffer.from(teamMembers_file.data.content, "base64").toString("utf-8")
+    );
+
+    console.log(teamMembers);
+
+    const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+    console.log(`GITHUB_TOKEN: ${GITHUB_TOKEN}`);
+    const { context = {} } = github;
+    const { pull_request } = context.payload;
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
